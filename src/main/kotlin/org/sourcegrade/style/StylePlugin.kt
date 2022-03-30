88f0951742a9065ede89da5e3cc2219fc99c0fc9
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-30 23:50:16 +0200

    Update checkstyle to v10.1

--- src/main/kotlin/org/sourcegrade/style/StylePlugin.kt
+++ src/main/kotlin/org/sourcegrade/style/StylePlugin.kt
@@ -18,7 +18,7 @@ class StylePlugin : Plugin<Project> {
         target.configure<CheckstyleExtension> {
             config = target.resources.text.fromString(checkStyleConfiguration.use { it.reader().readText() })
             maxWarnings = 0
-            toolVersion = "10.0"
+            toolVersion = "10.1"
         }
         target.apply<KtlintPlugin>()
         target.configure<KtlintExtension> {
